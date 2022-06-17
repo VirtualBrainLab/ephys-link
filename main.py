b3fe1d00009db1ab2cc44946814dd2ce356a47dc
Kenneth J. Yang <kjy5@uw.edu>
2022-06-17 15:33:20 -0700

    Documentation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import eventlet
+# noinspection PyPackageRequirements
 import socketio
 
 # Setup server
@@ -9,20 +10,24 @@ app = socketio.WSGIApp(sio)
 # Handle connection events
 @sio.event
 def connect(sid, environ, auth):
+    """Acknowledge connection to the server."""
     print('Connection: ', sid)
 
 
 @sio.event
 def disconnect(sid):
+    """Acknowledge disconnection from the server."""
     print('Disconnect: ', sid)
 
 
 # Message
 @sio.event
 def message(sid, data):
+    """Receive message from the a client and respond."""
     print(sid, ' says: ', data)
     sio.emit('message', [1, 2, 3, 4])
 
 
+# Start server
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
--- tests/server_test.py
+++ tests/server_test.py
@@ -1,3 +1,4 @@
+# noinspection PyPackageRequirements
 import socketio
 
 sio = socketio.Client()
@@ -6,20 +7,24 @@ sio = socketio.Client()
 # Connection events
 @sio.event
 def connect():
+    """Acknowledge connection to the server."""
     print('connection established')
 
 
 @sio.event
 def disconnect():
+    """Acknowledge disconnection from the server."""
     print('disconnected from server')
 
 
 # Message events
 @sio.event
 def message(data):
+    """Receive message from the server."""
     print('message received: ', data)
 
 
+# Connect to the server and send message
 sio.connect('http://localhost:5000')
 sio.emit('message', 'hello world')
 sio.wait()
