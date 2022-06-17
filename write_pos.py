b073cb7ed198ca82862eaf21ae2033bd6420b034
Kenneth J. Yang <kjy5@uw.edu>
2022-06-17 13:26:06 -0700

    Write position on two devices and have them sync up

--- write_pos.py
+++ write_pos.py
@@ -1,4 +1,7 @@
+import time
+
 from sensapex import UMP
+import time
 
 # Init and get devices 1 and 2
 ump = UMP.get_ump()
@@ -16,19 +19,20 @@ manipulator_2 = ump.get_device(2)
 
 # Move
 print("Moving to 0")
-move_1 = ump.goto_pos(2, [0, 0, 0, 0], 2000)
-# move_2 = ump.goto_pos(2, [0, 0, 0, 0], 2000)
+move_1 = ump.goto_pos(1, [0, 0, 0, 0], 2000)
+time.sleep(6)
+move_2 = ump.goto_pos(2, [0, 0, 0, 0], 2000)
 
 move_1.finished_event.wait()
-# move_2.finished_event.wait()
+move_2.finished_event.wait()
 print("Move finished")
 
 print("Moving back to home")
-move_1 = ump.goto_pos(2, [10000, 10000, 10000, 10000], 2000)
-# move_2 = ump.goto_pos(2, [10000, 10000, 10000, 10000], 2000)
+move_1 = ump.goto_pos(1, [10000, 10000, 10000, 10000], 2000)
+move_2 = ump.goto_pos(2, [10000, 10000, 10000, 10000], 2000)
 
 move_1.finished_event.wait()
-# move_2.finished_event.wait()
+move_2.finished_event.wait()
 print("Move finished")
 
 ump.close()
