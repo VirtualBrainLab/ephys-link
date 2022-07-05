b475c9b84e11cff3d5bdd2de607533ad1acf5554
Kenneth J. Yang <kjy5@uw.edu>
2022-07-05 13:26:18 -0700

    Read to file efficiently

--- scripts/read_to_file.py
+++ scripts/read_to_file.py
@@ -1,10 +1,7 @@
-import threading
-from collections import deque
 import csv
-import signal
 # noinspection PyPackageRequirements
 import socketio
-import sys
+import atexit
 
 import time
 
@@ -18,13 +15,15 @@ sio.emit('register_manipulator', 2)
 sio.emit('bypass_calibration', 2)
 
 running = True
+sleep_time = 1 / 120
+movement_threshold = 0.1
+
+prev_pos_1 = [-1, -1, -1, -1]
+prev_pos_2 = [-1, -1, -1, -1]
 
 # Setup CSV files
 man_1 = open('data/man_1.csv', 'a', newline='')
 man_2 = open('data/man_2.csv', 'a', newline='')
-
-man_1_data = deque()
-man_2_data = deque()
 
 man_1_writer = csv.writer(man_1)
 man_2_writer = csv.writer(man_2)
@@ -36,42 +35,50 @@ man_2_writer.writerow(header)
 
 def record(manipulator_id: int, pos: list[float], error: str):
     if error == '':
-        dest = man_1_data if manipulator_id == 1 else man_2_data
-        dest.appendleft([time.time()] + pos)
-        sio.emit('get_pos', manipulator_id, callback=record)
+        global prev_pos_1
+        global prev_pos_2
+
+        # Get files to work with
+        dest = man_1 if manipulator_id == 1 else man_2
+        writer = man_1_writer if manipulator_id == 1 else man_2_writer
+        prev_pos = prev_pos_1 if manipulator_id == 1 else prev_pos_2
+
+        # Check if update needed
+        should_write = False
+        for prev, cur in zip(prev_pos, pos):
+            if abs(prev - cur) > movement_threshold:
+                should_write = True
+                break
+
+        # Update if needed
+        if should_write:
+            writer.writerow([time.time()] + pos)
+            dest.flush()
+            if manipulator_id == 1:
+                prev_pos_1 = pos
+            else:
+                prev_pos_2 = pos
+
+        # Pull next position
+        if running:
+            time.sleep(sleep_time)
+            sio.emit('get_pos', manipulator_id, callback=record)
 
 
-def save():
-    while running:
-        if len(man_1_data):
-            man_1_writer.writerows(list(man_1_data))
-            man_1_data.clear()
-        if len(man_2_data):
-            man_2_writer.writerow(list(man_2_data))
-            man_2_data.clear()
+@atexit.register
+def cleanup(_, __):
+    print("Cleaning up")
+    global running
+    running = False
 
-    if len(man_1_data):
-        man_1_writer.writerows(list(man_1_data))
-    if len(man_2_data):
-        man_2_writer.writerow(list(man_2_data))
+    man_1.flush()
+    man_2.flush()
 
     man_1.close()
     man_2.close()
-    print("Finished saving")
-    sys.exit(0)
 
-
-def cleanup(_, __):
-    global running
-    running = False
-    print("Stopping")
     sio.disconnect()
-    sys.exit(0)
 
-
-signal.signal(signal.SIGINT, cleanup)
-
-threading.Thread(target=save).start()
 
 print("Begin recording")
 sio.emit('get_pos', 1, callback=record)
