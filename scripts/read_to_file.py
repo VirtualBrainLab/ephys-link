f50fb48342f86bcb1152bbd7b232eb2877b0d7ef
Kenneth J. Yang <kjy5@uw.edu>
2022-07-08 15:40:57 -0700

    General case setup with CLI

--- scripts/read_to_file.py
+++ scripts/read_to_file.py
@@ -1,50 +1,91 @@
+import argparse
 import csv
 # noinspection PyPackageRequirements
 import socketio
 import atexit
-
+import os
 import time
+
+# Parse arguments
+parser = argparse.ArgumentParser(description='Read and record position into '
+                                             'CSV files',
+                                 prog='python read_to_file.py')
+parser.add_argument('-i', '--ip', type=str, default='localhost', dest='ip',
+                    help='IP address to listen on')
+parser.add_argument('-p', '--port', type=int, default=8080, dest='port',
+                    help='Port to listen on')
+parser.add_argument('-m', '--manipulators', type=str, default='1,2',
+                    dest='manipulators',
+                    help='Manipulator IDs, separated by commas with no spaces')
+args = parser.parse_args()
+
+
+class ManipulatorMeta:
+    """Metadata for a manipulator"""
+
+    def __init__(self, man_id: int):
+        """
+        Construct a ManipulatorMeta object
+        :param man_id: Manipulator ID
+        """
+        # File
+        file_exists = os.path.exists(f'data/{man_id}.csv')
+        self.file = open(f'data/{man_id}.csv', 'a', newline='')
+        self.writer = csv.writer(self.file)
+        if not file_exists:
+            self.writer.writerow(['time (sec)', 'x', 'y', 'z', 'depth'])
+
+        # Previous position
+        self.prev_pos = [-1, -1, -1, -1]
+
+
+# Setup data directory
+if not os.path.exists('data'):
+    os.makedirs('data')
+
+# Extract manipulators
+manipulators = None
+try:
+    manipulators = {int(m): ManipulatorMeta(int(m)) for m in
+                    args.manipulators.split(',')}
+except ValueError:
+    print('Invalid manipulator IDs')
+    exit(1)
+except Exception as e:
+    print(e)
+    exit(1)
 
 # Setup sockets
 sio = socketio.Client()
-sio.connect('http://10.18.251.95:8080')
+try:
+    # noinspection HttpUrlsUsage
+    sio.connect(f'http://{args.ip}:{args.port}')
+except Exception as e:
+    print(f'Invalid IP address or port: {e}')
+    exit(1)
 
-sio.emit('register_manipulator', 1)
-sio.emit('bypass_calibration', 1)
-sio.emit('register_manipulator', 2)
-sio.emit('bypass_calibration', 2)
 
-# Setup CSV files
-man_1 = open('data/man_1.csv', 'a', newline='')
-man_2 = open('data/man_2.csv', 'a', newline='')
+def register_manipulators():
+    """Register and bypass calibration for all manipulators"""
+    for man_id in manipulators.keys():
+        sio.emit('register_manipulator', man_id)
+        sio.emit('bypass_calibration', man_id)
 
-man_1_writer = csv.writer(man_1)
-man_2_writer = csv.writer(man_2)
-
-header = ['time', 'x', 'y', 'z', 'depth']
-man_1_writer.writerow(header)
-man_2_writer.writerow(header)
 
 # Setup operations
 running = True
 sleep_time = 1 / 120
 movement_threshold = 0.1
-
-prev_pos_1 = [-1, -1, -1, -1]
-prev_pos_2 = [-1, -1, -1, -1]
-
 start_time = time.time()
 
 
-def record(manipulator_id: int, pos: list[float], error: str):
+def record(man_id: int, pos: list[float], error: str):
     if error == '':
-        global prev_pos_1
-        global prev_pos_2
-
         # Get files to work with
-        dest = man_1 if manipulator_id == 1 else man_2
-        writer = man_1_writer if manipulator_id == 1 else man_2_writer
-        prev_pos = prev_pos_1 if manipulator_id == 1 else prev_pos_2
+        manipulator = manipulators[man_id]
+        dest = manipulator.file
+        writer = manipulator.writer
+        prev_pos = manipulator.prev_pos
 
         # Check if update needed
         should_write = False
@@ -57,18 +98,15 @@ def record(manipulator_id: int, pos: list[float], error: str):
         if should_write:
             writer.writerow([time.time() - start_time] + pos)
             dest.flush()
-            if manipulator_id == 1:
-                prev_pos_1 = pos
-            else:
-                prev_pos_2 = pos
+            manipulator.prev_pos = pos
     elif error == 'Manipulator not registered':
-        sio.emit('register_manipulator', manipulator_id)
-        sio.emit('bypass_calibration', manipulator_id)
+        sio.emit('register_manipulator', man_id)
+        sio.emit('bypass_calibration', man_id)
 
     # Pull next position
     if running:
         time.sleep(sleep_time)
-        sio.emit('get_pos', manipulator_id, callback=record)
+        sio.emit('get_pos', man_id, callback=record)
 
 
 @atexit.register
@@ -77,15 +115,14 @@ def cleanup(_, __):
     global running
     running = False
 
-    man_1.flush()
-    man_2.flush()
-
-    man_1.close()
-    man_2.close()
+    for manipulator in manipulators:
+        manipulator.file.flush()
+        manipulator.file.close()
 
     sio.disconnect()
 
 
 print("Begin recording")
-sio.emit('get_pos', 1, callback=record)
-sio.emit('get_pos', 2, callback=record)
+for manipulator_id in manipulators.keys():
+    sio.emit('get_pos', manipulator_id, callback=record)
+    sio.emit('get_pos', manipulator_id, callback=record)
