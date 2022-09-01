31cf94796c269a5d32cd53c5ff1e5d8bfa8a6a9e
kjy5 <kjy5@users.noreply.github.com>
2022-09-01 00:51:59 +0000

    Autoformat code

--- ephys_link/__init__.py
+++ ephys_link/__init__.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-import common # pylint: disable=W0611, E402
-import sensapex_handler # pylint: disable=W0611, E402
-import sensapex_manipulator # pylint: disable=W0611, E402
-import server # pylint: disable=W0611, E402
+import common  # pylint: disable=W0611, E402
+import sensapex_handler  # pylint: disable=W0611, E402
+import sensapex_manipulator  # pylint: disable=W0611, E402
+import server  # pylint: disable=W0611, E402
