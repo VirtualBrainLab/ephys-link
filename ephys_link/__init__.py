abff8e389cac18083713911a242d9baca594d9a0
Kenneth Yang <82800265+kjy5@users.noreply.github.com>
2022-08-31 17:57:44 -0700

    skip file

--- ephys_link/__init__.py
+++ ephys_link/__init__.py
@@ -1,9 +1,10 @@
+# pylint: skip-file
 import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-import common  # pylint: disable=W0611,E402
-import sensapex_handler  # pylint: disable=W0611,E402
-import sensapex_manipulator  # pylint: disable=W0611,E402
-import server  # pylint: disable=W0611,E402
+import common
+import sensapex_handler
+import sensapex_manipulator
+import server
