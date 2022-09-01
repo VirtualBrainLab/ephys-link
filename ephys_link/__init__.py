da659eebc12cce4cfe81203a919440c872f30022
Kenneth Yang <82800265+kjy5@users.noreply.github.com>
2022-08-31 17:51:23 -0700

    Moved disable to per-line

--- ephys_link/__init__.py
+++ ephys_link/__init__.py
@@ -3,8 +3,7 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-# pylint: disable=W0611, E402
-import common
-import sensapex_handler
-import sensapex_manipulator
-import server
+import common # pylint: disable=W0611, E402
+import sensapex_handler # pylint: disable=W0611, E402
+import sensapex_manipulator # pylint: disable=W0611, E402
+import server # pylint: disable=W0611, E402
