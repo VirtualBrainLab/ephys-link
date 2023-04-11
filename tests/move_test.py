167c48b6745675bb837bca91308548c1da017aa2
Kenneth Yang <82800265+kjy5@users.noreply.github.com>
2023-04-11 14:47:41 -0700

    156 bug sensapex movement timeout (#157)
    * New test and maybe fix closing error
    
    * Revert back to polling on movement
    
    * Autoformat code
    
    ---------
    
    Co-authored-by: kjy5 <kjy5@users.noreply.github.com>

--- tests/move_test.py
+++ tests/move_test.py
@@ -1,3 +1,4 @@
+import threading
 from unittest import TestCase
 from unittest.mock import Mock
 
@@ -11,7 +12,7 @@ from ephys_link.common import PositionalOutputData
 class MoveTest(TestCase):
     """Tests movement event"""
 
-    DRIVE_SPEED = 2000
+    DRIVE_SPEED = 5000
 
     def setUp(self) -> None:
         """Setup test case"""
@@ -22,56 +23,76 @@ class MoveTest(TestCase):
 
     def test_move_no_asserts(self):
         """Test movement without asserts"""
-        self.sio.emit("register_manipulator", "0")
-        self.sio.emit("bypass_calibration", "0")
-        self.sio.emit("register_manipulator", "1")
-        self.sio.emit("bypass_calibration", "1")
+        # self.sio.emit("register_manipulator", "0")
+        # self.sio.emit("bypass_calibration", "0")
+        # self.sio.emit(
+        #     "set_can_write", {"manipulator_id": "0", "can_write": True, "hours": 1}
+        # )
+        # self.sio.emit("register_manipulator", "1")
+        # self.sio.emit("bypass_calibration", "1")
+        # self.sio.emit(
+        #     "set_can_write", {"manipulator_id": "1", "can_write": True, "hours": 1}
+        # )
         # self.sio.emit("register_manipulator", "2")
         # self.sio.emit("bypass_calibration", "2")
-        self.sio.emit(
-            "set_can_write", {"manipulator_id": "0", "can_write": True, "hours": 1}
-        )
-        self.sio.emit(
-            "set_can_write", {"manipulator_id": "1", "can_write": True, "hours": 1}
-        )
         # self.sio.emit(
         #     "set_can_write", {"manipulator_id": "2", "can_write": True, "hours": 1}
         # )
+        self.sio.emit("register_manipulator", "4")
+        self.sio.emit("bypass_calibration", "4")
+        self.sio.emit(
+            "set_can_write", {"manipulator_id": "4", "can_write": True, "hours": 1}
+        )
 
-        self.sio.emit(
-            "goto_pos",
-            {"manipulator_id": "0", "pos": [0, 0, 0, 0], "speed": self.DRIVE_SPEED},
-            callback=self.mock,
-        )
-        self.sio.emit(
-            "goto_pos",
-            {"manipulator_id": "1", "pos": [0, 0, 0, 0], "speed": self.DRIVE_SPEED},
-            callback=self.mock,
-        )
+        # Call get_pos() continuously
+        def get_pos():
+            pos_mock = Mock()
+            while True:
+                self.sio.emit("get_pos", "4", callback=pos_mock)
+                while not pos_mock.called:
+                    self.sio.sleep(0.1)
+
+        threading.Thread(target=get_pos, daemon=True).start()
+
+        # self.sio.emit(
+        #     "goto_pos",
+        #     {"manipulator_id": "0", "pos": [0, 0, 0, 0], "speed": self.DRIVE_SPEED},
+        #     callback=self.mock,
+        # )
+        # self.sio.emit(
+        #     "goto_pos",
+        #     {"manipulator_id": "1", "pos": [0, 0, 0, 0], "speed": self.DRIVE_SPEED},
+        #     callback=self.mock,
+        # )
         # self.sio.emit(
         #     "goto_pos",
         #     {"manipulator_id": "2", "pos": [0, 0, 0, 0], "speed": self.DRIVE_SPEED},
         #     callback=self.mock,
         # )
+        self.sio.emit(
+            "goto_pos",
+            {"manipulator_id": "4", "pos": [0, 0, 0, 0], "speed": self.DRIVE_SPEED},
+            callback=self.mock,
+        )
 
-        self.sio.emit(
-            "goto_pos",
-            {
-                "manipulator_id": "0",
-                "pos": [10, 10, 10, 10],
-                "speed": self.DRIVE_SPEED,
-            },
-            callback=self.mock,
-        )
-        self.sio.emit(
-            "goto_pos",
-            {
-                "manipulator_id": "1",
-                "pos": [10, 10, 10, 10],
-                "speed": self.DRIVE_SPEED,
-            },
-            callback=self.mock,
-        )
+        # self.sio.emit(
+        #     "goto_pos",
+        #     {
+        #         "manipulator_id": "0",
+        #         "pos": [10, 10, 10, 10],
+        #         "speed": self.DRIVE_SPEED,
+        #     },
+        #     callback=self.mock,
+        # )
+        # self.sio.emit(
+        #     "goto_pos",
+        #     {
+        #         "manipulator_id": "1",
+        #         "pos": [10, 10, 10, 10],
+        #         "speed": self.DRIVE_SPEED,
+        #     },
+        #     callback=self.mock,
+        # )
         # self.sio.emit(
         #     "goto_pos",
         #     {
@@ -81,8 +102,27 @@ class MoveTest(TestCase):
         #     },
         #     callback=self.mock,
         # )
+        self.sio.emit(
+            "goto_pos",
+            {
+                "manipulator_id": "4",
+                "pos": [20, 20, 20, 20],
+                "speed": self.DRIVE_SPEED,
+            },
+            callback=self.mock,
+        )
 
-        while self.mock.call_count != 4:
+        self.sio.emit(
+            "goto_pos",
+            {
+                "manipulator_id": "4",
+                "pos": [10, 10, 10, 10],
+                "speed": self.DRIVE_SPEED,
+            },
+            callback=self.mock,
+        )
+
+        while self.mock.call_count != 3:
             pass
 
     def test_move_with_asserts(self):
