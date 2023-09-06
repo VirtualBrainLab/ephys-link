82566b49c7b0b12a3dcf96513a50e2d9bb1a8db0
Kenneth Yang <kjy5@uw.edu>
2023-09-06 13:33:58 -0700

    Removed queue from New Scale

--- scripts/drive_test.py
+++ scripts/drive_test.py
@@ -5,13 +5,15 @@ import socketio
 sio = socketio.Client()
 sio.connect("http://localhost:8081")
 
-sio.emit("register_manipulator", "2")
-sio.emit("bypass_calibration", "2")
+manipulator_id = "0"
 
-sio.emit("set_can_write", {"manipulator_id": "2", "can_write": True, "hours": 1})
+sio.emit("register_manipulator", manipulator_id)
+sio.emit("bypass_calibration", manipulator_id)
 
-sio.emit("goto_pos", {"manipulator_id": "2", "pos": [10, 10, 10, 10], "speed": 4000})
+sio.emit("set_can_write", {"manipulator_id": manipulator_id, "can_write": True, "hours": 1})
+
+sio.emit("goto_pos", {"manipulator_id": manipulator_id, "pos": [10, 10, 10, 10], "speed": 4000})
 
 time.sleep(1)
 
-sio.emit("goto_pos", {"manipulator_id": "2", "pos": [0, 0, 0, 0], "speed": 4000})
+sio.emit("goto_pos", {"manipulator_id": manipulator_id, "pos": [0, 0, 0, 0], "speed": 4000})
