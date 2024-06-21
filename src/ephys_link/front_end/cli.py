c1f4b8d85c4bc7004ca4b2f35b9cef91241b3936
Kenneth Yang <kjy5@uw.edu>
2024-06-21 16:52:14 -0700

    Reset with new CLI parser

---
+++ src/ephys_link/front_end/cli.py
@@ -0,0 +1,88 @@
+"""Command-line interface for the Electrophysiology Manipulator Link.
+
+Declaration of the CLI with options and getters for the parsed arguments.
+"""
+
+from argparse import ArgumentParser
+
+from vbl_aquarium.models.ephys_link import EphysLinkOptions
+
+from ephys_link.__about__ import __version__ as version
+
+
+class CLI:
+    def __init__(self) -> None:
+        """Initialize CLI parser."""
+
+        self._parser = ArgumentParser(
+            description="Electrophysiology Manipulator Link: a Socket.IO interface for manipulators in electrophysiology experiments.",
+            prog="python -m ephys-link",
+        )
+
+        self._parser.add_argument(
+            "-b", "--background", dest="background", action="store_true", help="Skip configuration window."
+        )
+        self._parser.add_argument(
+            "-i",
+            "--ignore-updates",
+            dest="ignore_updates",
+            action="store_true",
+            help="Skip (ignore) checking for updates.",
+        )
+        self._parser.add_argument(
+            "-t",
+            "--type",
+            type=str,
+            dest="type",
+            default="ump-4",
+            help='Manipulator type (i.e. "ump-4", "ump-3", "new_scale", or "pathfinder_mpm"). Default: "ump-4".',
+        )
+        self._parser.add_argument("-d", "--debug", dest="debug", action="store_true", help="Enable debug mode.")
+        self._parser.add_argument("-x", "--use-proxy", dest="use_proxy", action="store_true", help="Enable proxy mode.")
+        self._parser.add_argument(
+            "-a",
+            "--proxy-address",
+            type=str,
+            default="proxy2.virtualbrainlab.org",
+            dest="proxy_address",
+            help="Proxy IP address.",
+        )
+        self._parser.add_argument(
+            "-p",
+            "--port",
+            type=int,
+            default=8081,
+            dest="port",
+            help="TCP/IP port to use. Default: 8081 (avoids conflict with other HTTP servers).",
+        )
+        self._parser.add_argument(
+            "--mpm_port",
+            type=int,
+            default=8080,
+            dest="mpm_port",
+            help="Port New Scale Pathfinder MPM's server is on. Default: 8080.",
+        )
+        self._parser.add_argument(
+            "-s",
+            "--serial",
+            type=str,
+            default="no-e-stop",
+            dest="serial",
+            nargs="?",
+            help="Emergency stop serial port (i.e. COM3). Default: disables emergency stop.",
+        )
+        self._parser.add_argument(
+            "-v",
+            "--version",
+            action="version",
+            version=f"Electrophysiology Manipulator Link v{version}",
+            help="Print version and exit.",
+        )
+
+    def parse_args(self) -> EphysLinkOptions:
+        """Parse arguments and return them
+
+        :returns: Parsed arguments
+        :rtype: EphysLinkOptions
+        """
+        return EphysLinkOptions(**vars(self._parser.parse_args()))
