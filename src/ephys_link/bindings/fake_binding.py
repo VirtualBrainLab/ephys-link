625c8824e5f149cc7dd8cc69d0ba14ff817660e7
Kenneth Yang <kjy5@uw.edu>
2025-01-04 19:33:09 -0800

    Fixed fake

--- src/ephys_link/bindings/fake_binding.py
+++ src/ephys_link/bindings/fake_binding.py
@@ -1,9 +1,12 @@
+from typing import final, override
+
 from vbl_aquarium.models.unity import Vector3, Vector4
 
 from ephys_link.utils.base_binding import BaseBinding
 from ephys_link.utils.common import list_to_vector4
 
 
+@final
 class FakeBinding(BaseBinding):
     def __init__(self) -> None:
         """Initialize fake manipulator infos."""
@@ -21,47 +24,61 @@ class FakeBinding(BaseBinding):
         ]
 
     @staticmethod
+    @override
     def get_display_name() -> str:
         return "Fake Manipulator"
 
     @staticmethod
+    @override
     def get_cli_name() -> str:
         return "fake"
 
+    @override
     async def get_manipulators(self) -> list[str]:
         return list(map(str, range(8)))
 
+    @override
     async def get_axes_count(self) -> int:
         return 4
 
+    @override
     def get_dimensions(self) -> Vector4:
         return list_to_vector4([20] * 4)
 
+    @override
     async def get_position(self, manipulator_id: str) -> Vector4:
         return self._positions[int(manipulator_id)]
 
+    @override
     async def get_angles(self, manipulator_id: str) -> Vector3:
         return self._angles[int(manipulator_id)]
 
-    async def get_shank_count(self, _: str) -> int:
+    @override
+    async def get_shank_count(self, manipulator_id: str) -> int:
         return 1
 
+    @override
     def get_movement_tolerance(self) -> float:
         return 0.001
 
-    async def set_position(self, manipulator_id: str, position: Vector4, _: float) -> Vector4:
+    @override
+    async def set_position(self, manipulator_id: str, position: Vector4, speed: float) -> Vector4:
         self._positions[int(manipulator_id)] = position
         return position
 
-    async def set_depth(self, manipulator_id: str, depth: float, _: float) -> float:
+    @override
+    async def set_depth(self, manipulator_id: str, depth: float, speed: float) -> float:
         self._positions[int(manipulator_id)].w = depth
         return depth
 
-    async def stop(self, _: str) -> None:
+    @override
+    async def stop(self, manipulator_id: str) -> None:
         pass
 
+    @override
     def platform_space_to_unified_space(self, platform_space: Vector4) -> Vector4:
         return platform_space
 
+    @override
     def unified_space_to_platform_space(self, unified_space: Vector4) -> Vector4:
         return unified_space
