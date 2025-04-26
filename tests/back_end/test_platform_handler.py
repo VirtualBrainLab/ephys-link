4aa5caa2b9f32176d94af00e70c4ce8a1eb32ff9
Kenneth Yang <kjy5@uw.edu>
2025-04-26 11:51:30 -0700

    Test get_display_name

--- tests/back_end/test_platform_handler.py
+++ tests/back_end/test_platform_handler.py
@@ -5,38 +5,29 @@ from pytest_mock import MockerFixture
 from vbl_aquarium.models.ephys_link import EphysLinkOptions
 
 from ephys_link.back_end.platform_handler import PlatformHandler
+from ephys_link.utils.base_binding import BaseBinding
+from ephys_link.utils.console import Console
 
 
 class TestPlatformHandler:
     """Tests for the PlatformHandler class."""
 
-    @pytest.fixture
-    def mock_platform_handler(self, mock_console: MagicMock, mocker: MockerFixture) -> PlatformHandler:
-        """Fixture for the PlatformHandler class.
+    def test_get_display_name(self, mock_console: Console, mocker: MockerFixture) -> None:
+        """Platform should return binding display name.
 
         Args:
-            mock_console: Mocked Console class.
-            mocker: Mock fixture for the binding class.
-
-        Returns:
-            Mocked PlatformHandler class.
+            mock_console: Mocked Console instance.
+            mocker: Binding mocker.
         """
-        # Create dummy options.
-        EphysLinkOptions(
-            type="dummy"
-        )
-
-        # Patch get_bindings to return a dummy binding.
-
+        # Define dummy name.
+        dummy_name = "Dummy Binding"
+        
+        # Mock binding.
+        mock_binding = mocker.Mock(spec=BaseBinding)
+        mock_binding.get_display_name.return_value = dummy_name
+        
         # Create PlatformHandler instance.
-        return PlatformHandler(self.options, mock_console)
-
-    def test_get_display_name(self, mock_platform_handler: PlatformHandler) -> None:
-        """Test the get_display_name method.
-
-        Args:
-            mock_platform_handler: Mocked PlatformHandler class.
-        """
-        mock_platform_handler.get_display_name()
-
-        self.binding.assert_called()
+        platform_handler = PlatformHandler(mock_binding, mock_console)
+        
+        # Test.
+        assert platform_handler.get_display_name() == dummy_name
