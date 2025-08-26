f106ef728af9adc66907795b47b0594df31975a1
Kenneth Yang <kjy5@uw.edu>
2025-08-26 16:54:43 -0700

    Multi iteration

--- scripts/jackhammer.py
+++ scripts/jackhammer.py
@@ -1,10 +1,10 @@
+from ctypes import c_char, c_int
+
 from sensapex import UMP
-from ctypes import c_int, c_char
 
 um = UMP.get_ump()
 
 dev_id = 3
 axis = 3
 
-for i in range(20):
-    um.call("um_take_jackhammer_step", c_int(dev_id), c_char(axis), c_int(1), c_int(1), c_int(50), c_int(0), c_int(0))
+um.call("um_take_jackhammer_step", c_int(dev_id), c_char(axis), c_int(20), c_int(10), c_int(50), c_int(5), c_int(-50))
