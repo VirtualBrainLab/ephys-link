b6d1f10dfbc46bacd8acf60732a521b588b1e4f6
Kenneth Yang <kjy5@uw.edu>
2025-08-27 09:21:11 -0700

    Added comments

--- scripts/jackhammer.py
+++ scripts/jackhammer.py
@@ -7,4 +7,11 @@ um = UMP.get_ump()
 dev_id = 3
 axis = 3
 
-um.call("um_take_jackhammer_step", c_int(dev_id), c_char(axis), c_int(20), c_int(10), c_int(50), c_int(5), c_int(-50))
+um.call("um_take_jackhammer_step",
+        c_int(dev_id), # Device number
+        c_char(axis), # Axis (0=X, 1=Y, 2=Z, 3=D)
+        c_int(20), # Number of cycles
+        c_int(10), # Number of steps in first-stage
+        c_int(15), # Thrust length
+        c_int(5), # Number of steps in second-stage
+        c_int(-15)) # Thrust length
