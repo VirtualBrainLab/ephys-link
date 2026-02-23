7c4b921804396f5a5cf24f5f3981b8799c9e95c9
Zhorzh Zelenkov <zelenkovh@gmail.com>
2026-02-23 15:13:39 -0800

    Saving progress before implementing closed loop jackhammer

--- scripts/test_jackhammer.py
+++ scripts/test_jackhammer.py
@@ -1,10 +1,14 @@
 from socketio import SimpleClient
 
+
+#  .\venv\Scripts\Activate.ps1
+#  ephys-link -b -t ump
+#  python scripts/test_jackhammer.py
 sio = SimpleClient()
 sio.connect("http://localhost:3000")
 result = sio.call("jackhammer", '''{
-    "manipulator_id": "6",
-    "iterations": 3,
+    "manipulator_id": "4",
+    "iterations": 2,
     "phase1_steps": 1,
     "phase1_pulses": 80,
     "phase2_steps": 1,
